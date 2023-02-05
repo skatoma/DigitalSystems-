ee8c1e2fc2cbd2e8c7aff797e2b4f727f55aaa35
skatoma <91471411+skatoma@users.noreply.github.com>
2023-02-05 15:36:49 +0000

    Update mul_tb.sv

--- tasks/Task222-MulDiv/mul_tb.sv
+++ tasks/Task222-MulDiv/mul_tb.sv
@@ -12,6 +12,28 @@ mul m1(Y,A,B);
 initial
 begin
 	//Write test code here
+//testing lower extreme
+A=4d0;
+B=4d0;
+//Add a delay so that the outputs update
+		#10ps;
+assert (Y==8d0)
+ $display("correct %d * %d is %d",A,A,Y);
+else 
+ $display("error %d * %d is not %d",A,A,Y);
+
+  
+//testing upper extreme
+A=4d15;
+B=4d15;
+//Add a delay so that the outputs update
+		#10ps;
+assert (Y==8d225)
+ $display("correct %d * %d is %d",A,A,Y);
+else 
+ $display("error %d * %d is not %d",A,A,Y);
+
+
 end
 
 endmodule
